c6c3933c9df4fad97e0664f11b77e82c9a902fd4
DWalle03 <didrikbunes@outlook.com>
2025-01-28 22:40:11 +0100

    button id, array

--- Arbeidskrav_1/script.js
+++ Arbeidskrav_1/script.js
@@ -1,8 +1,9 @@
+const categories = ["HTML", "CSS", "JavaScript", "React", "Sanity and headless CMS" ];
+
 let resourcesHTML = ""
 
 resources.map((resource, category) => resourcesHTML +=
+        ``)
 
+document.getElementById("content").innerHTML = resourcesHTML; 
 
-)
-
-document.getElementById("content").innerHTML = resourcesHTML
